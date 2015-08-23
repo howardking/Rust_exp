e7ca93c413b4d62e96aff9c45c689aeeb20f0019
HowardKing <rjxx007@sina.com>
2015-08-23 21:06:58 +0800

    Rust's function

--- projects/function/src/main.rs
+++ projects/function/src/main.rs
@@ -3,8 +3,22 @@ fn main() {
 	let y = 100;
     print_sum(5);
 	println!("y is {}", y);
+	println!("------------------");
+	let tuple = (1, 2, 3);
+	let x = tuple.0;
+	let y = tuple.1;
+	let z = tuple.2;
+	println!("x = {}", x);
+	println!("y = {}", y);
+	println!("z = {}", z);
+	println!("------------------");
+	println!("h(5) = {}",h(5));
+
 }
 
 fn print_sum(x : i32){
 	println!("x is {}", x);
 }
+
+fn foo(x: i32) -> i32 { x }
+let h: fn(i32) -> i32 = foo;
